d5bf88e5c0e037e44e5353c59cf6cd9c9a40f31f
Edwin Joel Ulloa <edwinjoel1984@gmail.com>
2019-04-05 12:06:45 -0500

    example graphql query and mutation

---
+++ src/resolvers.js
@@ -0,0 +1,19 @@
+import { tasks }  from './sample';
+export const resolvers = {
+    Query: {
+        hello: () => {return "Hello World with GraphQL"},
+        greet(root,{name}){
+            return `Hello ${name}`;
+        },
+        tasks(){
+            return tasks;
+        },        
+    },
+    Mutation:{
+        createTask(_,{input}){
+            input._id = tasks.length
+            tasks.push(input)
+            return input;
+        }
+    }
+};
